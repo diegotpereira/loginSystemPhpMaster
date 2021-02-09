b5b3b812db06af6823f0aa7549aee87081b6f219
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-02-09 17:36:26 -0300

    criação do arquivo home.php

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <?php include ('header.php'); ?>
+</head>
+<body>
+    <div class="container text-center p-5 m-5">
+        <div class="p-5 display-1 text-center" style="font-family: 'Sigmar One', cursive;">Olá, Sr</div>
+        <div class="p-5 display-4 text-center" style="font-family: 'Sigmar One', cursive;">Como vai?</div>
+        <div class="btn btn-primary btn-outline-primary rounded-pill btn-lg font-weigth-bolder">
+            <a href="logout.php" class="text-white text-center"><p>Logout</p></a>
+        </div>    
+    </div>
+</body>
+</html>
